4e389cd951f02737fbf8ef222dd843b1dab11113
Atilla Berk Demir <74289559+Polo-cpu@users.noreply.github.com>
2024-05-01 22:57:20 +0300

    Exception package created with GenericExceptionHandler, VehicleNotCreatedException and VehicleNotFoundException classes.

---
+++ vehicle-service/src/main/java/com/vehiclerepair/vehicleservice/exception/VehicleNotCreatedException.java
@@ -0,0 +1,21 @@
+package com.vehiclerepair.vehicleservice.exception;
+
+import com.vehiclerepair.vehicleservice.model.enums.Language;
+import com.vehiclerepair.vehicleservice.response.MessageResponse;
+import com.vehiclerepair.vehicleservice.utils.IMessageCodes;
+import com.vehiclerepair.vehicleservice.utils.MessageUtils;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+public class VehicleNotCreatedException extends RuntimeException{
+    private final Language language;
+    private final IMessageCodes messageCodes;
+    public VehicleNotCreatedException(Language language,IMessageCodes messageCodes){
+        super(MessageUtils.getMessage(language,messageCodes));
+        this.language = language;
+        this.messageCodes = messageCodes;
+        log.error("[VehicleNotCreatedException] -> message: {}, developer message: {}",MessageUtils.getMessage(language,messageCodes),messageCodes);
+    }
+}
--- vehicle-service/src/main/java/com/vehiclerepair/vehicleservice/response/InternalServiceResponse.java
+++ vehicle-service/src/main/java/com/vehiclerepair/vehicleservice/response/InternalServiceResponse.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Data
 @Builder
 public class InternalServiceResponse<T> {
+    private MessageResponse messageResponse;
     private T payload;
     private List<T> payloadList;
     private boolean hasError;
